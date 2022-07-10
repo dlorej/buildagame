6447e71e4d7358f470546ac3141bd4ec7fb281e6
Dlorej <jeroldlimjunpin@gmail.com>
2022-07-11 03:10:30 +0800

    testing css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import hogback from "./hogwartsClassroom.jpg";
+import styles from "./styles.module.css";
 
 function App() {
     return (
         <div id="mainscreen">
-            <img id="currentBackground" src={hogback}></img>
+            <img
+                id="currentBackground"
+                src={hogback}
+                classname={styles.mainBackgroundSetting}
+            ></img>
         </div>
     );
 }
