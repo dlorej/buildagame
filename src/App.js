efdb7e4a7c244d966ca239593c3810a06aef5fee
Dlorej <jeroldlimjunpin@gmail.com>
2022-07-16 19:23:33 +0800

    scan qr code working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, Component } from "react";
 import "./App.css";
 import hogback from "./hogwartsClassroom.jpg";
 import styles from "./styles.module.css";
+import jsQR from "jsqr";
 
 function App() {
     //react doesnt support srcobject, need to use ref, ref not supported in functional components, need to use
     const StreamCam = () => {
+        const [decodedData, setDecodedData] = useState("empty");
         const playerRef = useRef();
+        const canvasRef = useRef();
         useEffect(() => {
             console.log("using effect");
             navigator.mediaDevices
@@ -18,8 +21,49 @@ function App() {
                     console.log("Something went wrong!");
                 });
         });
+        useEffect(() => {
+            setInterval(() => {
+                var context = canvasRef.current.getContext("2d");
+                context.drawImage(
+                    playerRef.current,
+                    0,
+                    0,
+                    playerRef.current.width,
+                    playerRef.current.height
+                );
+                var imageData = context.getImageData(
+                    0,
+                    0,
+                    playerRef.current.width,
+                    playerRef.current.height
+                );
+                var decodedDataVar = jsQR(
+                    imageData.data,
+                    playerRef.current.width,
+                    playerRef.current.height
+                );
+                if (decodedDataVar) {
+                    setDecodedData(decodedDataVar.data);
+                }
+            }, 500);
+        });
         return (
-            <video autoplay="true" id="videoElement" ref={playerRef}></video>
+            <div>
+                <video
+                    autoplay="true"
+                    id="videoElement"
+                    ref={playerRef}
+                    width="640"
+                    height="480"
+                ></video>
+                <canvas
+                    className={styles.hidden}
+                    ref={canvasRef}
+                    width="640"
+                    height="480"
+                ></canvas>
+                <p>{decodedData}</p>
+            </div>
         );
     };
     return (
